e220389f0908db60b352622cde56ba2154a3a2dc
abdellah <abril.abdellah@gmail.com>
2020-05-31 14:25:43 +0100

    Créez et exposez une API de la liste des produits et désactivez toutes les méthodes sauf Get

---
+++ src/main/java/com/abril/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,26 @@
+package com.abril.ecommerce.config;
+
+import com.abril.ecommerce.entity.Product;
+import com.abril.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] unsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+        //Désactiver les méthodes Post, Put, Delete pour Product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+        //Désactiver les méthodes Post, Put, Delete pour ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+    }
+}
